fd74686848179844f47703f41dbc6cd317e10c40
Auston Stewart <auston@shedbuilt.net>
2018-10-25 21:24:54 -1000

    Updating to v1.9.0

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# Configure
 ./configure --prefix=/usr \
-            --with-gobject && \
-make -j $SHED_NUM_JOBS && \
+            --with-gobject &&
+
+# Build and Install
+make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install
